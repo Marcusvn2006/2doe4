287412b2bf9313564003b414e1031eab8af989af
Marcus Vinicius <marcusavanso06@gmail.com>
2026-05-08 12:24:35 -0300

    feat: implement app routing with header and footer layout excluding maintenance page

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,15 @@ import './App.css'
 
 function Layout() {
   const { pathname } = useLocation()
-  const isManutencao = pathname === '/'
+  const isManutencao = pathname === '/manutencao'
 
   return (
     <>
       {!isManutencao && <Header />}
       <main className="overflow-x-clip">
         <Routes>
-          <Route path="/" element={<Manutencao />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/manutencao" element={<Manutencao />} />
           <Route path="/saude-brotas" element={<SaudeBrotas />} />
           <Route path="/equoterapia-boituva" element={<EquoterapiaBoituva />} />
           <Route path="/esporte-jau" element={<EsporteJau />} />
